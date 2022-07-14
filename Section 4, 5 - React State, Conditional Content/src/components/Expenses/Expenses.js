8ea6b5482447856b405583b273cebf51d8142589
JimmyWanHK <wankaho325@gmail.com>
2022-07-14 10:54:37 +0800

    completed 104/497

---
+++ Section 4, 5 - React State, Conditional Content/src/components/Expenses/Expenses.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import ExpensesFilter from "./ExpensesFilter";
+import Card from "../UI/Card";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+import "./Expenses.css";
+
+function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const filterChangeHandler = (value) => {
+    setFilteredYear(value);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
+
+        {/* {filteredExpenses.length === 0 && expensesContent}
+        {filteredExpenses.length > 0 &&
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))} */}
+      </Card>
+    </div>
+  );
+}
+
+export default Expenses;
