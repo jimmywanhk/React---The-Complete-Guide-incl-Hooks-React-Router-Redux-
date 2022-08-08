539ec809ca444c4bcee092f9f99040ba819796a3
JimmyWanHK <wankaho325@gmail.com>
2022-08-08 17:39:19 +0800

    Completed Section 19

---
+++ Section 19/src/App.js
@@ -0,0 +1,93 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import Notification from "./components/UI/Notification";
+import { sendCartData, fetchCartData } from "./store/cart-action";
+import { uiActions } from "./store/ui-slice";
+
+let isInitial = true;
+
+function App() {
+  const dispatch = useDispatch();
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
+
+  useEffect(() => {
+    //method 1: Inseide the components (e.g. useEffect())
+    // const sendCartData = async () => {
+    //   dispatch(
+    //     uiActions.showNotification({
+    //       status: "pending",
+    //       title: "Sending...",
+    //       message: "Sending cart data!",
+    //     })
+    //   );
+    //   const response = await fetch(
+    //     "https://react-http-2f283-default-rtdb.firebaseio.com/cart.json",
+    //     {
+    //       method: "PUT",
+    //       body: JSON.stringify(cart),
+    //     }
+    //   );
+    //   if (!response.ok) {
+    //     throw new Error("Sending cart data failed.");
+    //   }
+    //   const responseData = await response.json();
+    //   dispatch(
+    //     uiActions.showNotification({
+    //       status: "success",
+    //       title: "Success!",
+    //       message: "Sent cart data successfully!",
+    //     })
+    //   );
+    // };
+    // if (isInitial) {
+    //   isInitial = false;
+    //   return;
+    // }
+    // sendCartData().catch((error) => {
+    //   dispatch(
+    //     uiActions.showNotification({
+    //       status: "error",
+    //       title: "Error!",
+    //       message: "Sending cart data failed!",
+    //     })
+    //   );
+    // });
+
+    //method 2: Inside the action creators
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
+  }, [cart, dispatch]);
+
+  return (
+    <>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </>
+  );
+}
+
+export default App;
